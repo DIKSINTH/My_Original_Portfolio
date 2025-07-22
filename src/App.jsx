7c056455c27351125d69ee81daa681bf2b8176cf
DIKSINTH <diksinth1@gmail.com>
2025-07-22 13:19:11 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Sample from "./sample.jsx";
+function App() {
+  return (
+    <>
+      <Sample />
+      <h3>Tailwind With React Vite</h3>
+    </>
+  );
+}
+
+export default App;
---
+++ src/sample.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sample() {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold underline ">sample</h2>
+    </div>
+  );
+}
+
+export default Sample;
